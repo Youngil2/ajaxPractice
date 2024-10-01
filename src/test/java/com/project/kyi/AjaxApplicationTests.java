84cee59ac3abb98c7d063ff8e76649babc7a703b
김영일 <>
2024-10-01 16:45:15 +0900

    최초커밋

---
+++ src/test/java/com/project/kyi/AjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
